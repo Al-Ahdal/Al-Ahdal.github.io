9541375367b5615b2eee6fb794182874efb1059d
Al-Ahdal <ahmedlpu20@gmail.com>
2025-12-01 21:41:40 +0300

    3 commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -77,11 +77,11 @@ const HeroSection = ({ t }: HeroSectionProps) => {
       </div>
 
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
+      {/* <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-muted rounded-full flex justify-center">
           <div className="w-1 h-3 bg-primary rounded-full mt-2" />
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
